5538009079376d4588efe6b453c1a0448e46076a
Yongjia2 <166165053+Yongjia2@users.noreply.github.com>
2024-07-30 01:00:56 +1000

    Update dgm.py
    modify

--- dgm.py
+++ dgm.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy import linalg
 
 
-def auxfunc(sample, data, nrecord, min_dis_sample, w_record): # 已加入权重w
+def auxfunc(sample, data, nrecord, min_dis_sample, w_record): 
     iact = np.zeros((nrecord, 1))
     dist1 = np.array([np.linalg.norm(sample - tmp2) ** 2 for tmp2 in data])
     f_auxfunc = 0
@@ -15,23 +15,11 @@ def auxfunc(sample, data, nrecord, min_dis_sample, w_record): # 已加入权重w
         else:
             iact[i] = 2
             f_auxfunc += dist1[i]*w_record[i]
-    #iact_ff = iact.T
-    # with open('./results/test.txt', 'a') as file:
-    #     file.write(f'\t auxfunc_f,\t sample\t {f_auxfunc}\t{sample[0]}\t {sample[1]}\t {sample[2]}\t {sample[3]}\n')
-    # with open('./results/test.txt', 'a') as file:
-    #     file.write(
-    #             f'\t auxfunc_mindis\t {min_dis_sample[0]}\t {min_dis_sample[1]}\t {min_dis_sample[2]}\t '
-    #             f'{min_dis_sample[3]}\n')
-    # with open('./results/test.txt', 'a') as file:
-    #     file.write(
-    #             f'\t auxfunc_dist1\t {dist1[0]}\t {dist1[1]}\t {dist1[2]}\t {dist1[3]}\t\n')
-    # with open('./results/test.txt', 'a') as file:
-    #     file.write(f'\t auxfunc_iact\t sample\t {iact_ff[0,0:40]}\n')
+
     return f_auxfunc, iact
 
 
-def func(sample, data, nrecord, nc, w_record): # 已加入权重w
-    #print('ns=2时func')
+def func(sample, data, nrecord, nc, w_record): 
     mf = np.size(data, 1)
     lact3 = np.zeros((nrecord, 1))
     dist2 = np.zeros((nrecord, nc))
@@ -47,7 +35,7 @@ def func(sample, data, nrecord, nc, w_record): # 已加入权重w
     return f, lact3
 
 
-def fv(ns, sample, data, min_dis_sample, nrecord, nc, w_record):  # 已加入权重w
+def fv(ns, sample, data, min_dis_sample, nrecord, nc, w_record):  
     iact = np.zeros((nrecord, 1))
     lact3 = np.zeros((nrecord, 1))
     f2 = 0
@@ -59,16 +47,9 @@ def fv(ns, sample, data, min_dis_sample, nrecord, nc, w_record):  # 已加入权
     return f2, iact, lact3
 
 
-def auxgrad(sample, nrecord, iact, data, w_record): # 已加入权重w
+def auxgrad(sample, nrecord, iact, data, w_record):
     mf = np.size(data, 1)
     grad = np.zeros((mf, ))
-    # with open('./results/test.txt', 'a') as file:
-    #     file.write(f'\t grad\tsample\t {grad[0]}\t {grad[1]}\t {grad[2]}\t {sample[0]}\t {sample[1]}\t {sample[2]}\n')
-    # iact_f = iact.T
-    # with open('./results/test.txt', 'a') as file:
-    #     file.write(f'\t auxgrad_iact\t {iact_f[0,0:80]}\t\n')
-    # with open('./results/test.txt', 'a') as file:
-    #     file.write(f'\t auxgrad_iact\t {iact_f[0,120:180]}\t\n')
     for i in range(0, nrecord):
         if iact[i] == 2:
             for j in range(0, mf):
@@ -76,8 +57,7 @@ def auxgrad(sample, nrecord, iact, data, w_record): # 已加入权重w
 
     return grad
 
-
-def funcgrad(sample, nrecord, data, lact3, m, w_record): # 已加入权重w
+def funcgrad(sample, nrecord, data, lact3, m, w_record): 
     #print('ns=2时funcgrad')
     mf = np.size(data, 1)
     grad = np.zeros((m, ))
@@ -90,7 +70,7 @@ def funcgrad(sample, nrecord, data, lact3, m, w_record): # 已加入权重w
     return grad
 
 
-def dgrad(x, sl, g, iact, lact3, data, nrecord, ns, m, w_record): # 已加入权重w
+def dgrad(x, sl, g, iact, lact3, data, nrecord, ns, m, w_record): 
     x1 = np.empty_like(x)
     for k in range(0, m):
         x1[k] = x[k] + sl * g[k]
@@ -103,8 +83,7 @@ def dgrad(x, sl, g, iact, lact3, data, nrecord, ns, m, w_record): # 已加入权
     return grad
 
 
-def armijo(sample, data, min_dis_sample, nrecord, g, f1, f4, sl, ns, r, nc, m, w_record):  # 已加入权重w
-    #print('armijo')
+def armijo(sample, data, min_dis_sample, nrecord, g, f1, f4, sl, ns, r, nc, m, w_record):  
     step = sl
     f5 = f4
     x1 = np.zeros((m,))
@@ -120,13 +99,11 @@ def armijo(sample, data, min_dis_sample, nrecord, g, f1, f4, sl, ns, r, nc, m, w
             step = step/2.0e+00
             break
         f5 = f6
-    # with open('./results/test.txt', 'a') as file:
-    #    file.write(f'\t armijo_f5\t sample\t {f5}\n')
 
     return f5, step, iact, lact3
 
 
-def equations(n, a): # 不用加入权重w
+def equations(n, a):
     b = a[0:n, 0:n]
     e = np.ones((n, 1))
     z1 = linalg.solve(b, e)
@@ -135,7 +112,7 @@ def equations(n, a): # 不用加入权重w
     return z1
 
 
-def wolfe(ndg, prod, kmin, ns):  # 不用加入权重w
+def wolfe(ndg, prod, kmin, ns): 
     ij = np.zeros((10000, 1), dtype=int)
     ij[0] = kmin
     z = np.zeros((10000, 1))
@@ -193,27 +170,15 @@ def wolfe(ndg, prod, kmin, ns):  # 不用加入权重w
                 for j in range(0, jvertex):
                     a[i, j] = 1.0e00 + prod[ij[i]-1, ij[j]-1]
             j9 = j9 + 1
-            # if ns == 2:
-            #     with open('./results/test.txt', 'a') as file:
-            #         file.write(f'\t wolfer_j9\t {j9}\t\n')
             if j9 > jmax:
                 outer_break_flag = True
                 break
             z1 = equations(jvertex, a)
-            # if ns == 2:
-            #     with open('./results/test.txt', 'a') as file:
-            #         file.write(f'\t wolfer_z1\t {z1[0]}\t\n')
             while True:
                 if (z1[0:jvertex] <= 1.0e-10).any():
-                    # if ns == 2:
-                    #     with open('./results/test.txt', 'a') as file:
-                    #         file.write(f'\t wolfer_break1\t \n')
                     break
                 else:
                     z[0:jvertex] = z1[0:jvertex]
-                    # if ns == 2:
-                    #     with open('./results/test.txt', 'a') as file:
-                    #         file.write(f'\t wolfer_break_z1\t {z[0]}\t\n')
                     break_flag = True
                     break
             if break_flag:
@@ -236,19 +201,13 @@ def wolfe(ndg, prod, kmin, ns):  # 不用加入权重w
                     ij[j2-1] = ij[i]
                     z[j2-1] = z[i]
             jvertex = j2
-            # if ns == 2:
-            #     with open('./results/test.txt', 'a') as file:
-            #         file.write(f'\t wolfer_jvertex\t {jvertex}\t\n')
 
         if outer_break_flag:
             break
     return z, jvertex, ij
 
 
-def dgm(data, m, sample, min_dis_sample, ns, nc, w_record, nblocks): # 已加入权重w
-    # if nblocks == 2:
-    #     with open('./results/test.txt', 'a') as file:
-    #         file.write(f' dgm_sample_input: {sample} \t \r\n')
+def dgm(data, m, sample, min_dis_sample, ns, nc, w_record): 
     nrecord = np.size(data, 0)
     div = 1.0e-1
     slinit = 1.0e0
@@ -268,14 +227,7 @@ def dgm(data, m, sample, min_dis_sample, ns, nc, w_record, nblocks): # 已加入
     ww = np.zeros((1000, m))
     sl = slinit/div
 
-    # if ns == 2 and nc == 3:
-    #     with open('./results/test.txt', 'a') as file:
-    #         file.write(f'\t dem_sample_input\tg\t {sample} \n')
-
     f2, iact, lact3 = fv(ns, sample, data, min_dis_sample, nrecord, nc, w_record)
-    # if nblocks == 2:
-    #     with open('./results/test.txt', 'a') as file:
-    #         file.write(f' dgm_f2: {f2} \t \r\n')
 
     while True:
         sl = div * sl
@@ -284,18 +236,15 @@ def dgm(data, m, sample, min_dis_sample, ns, nc, w_record, nblocks): # 已加入
         for i in range(m):
             g[i] = 1.0e+00 / np.sqrt(m)
         nnew = 0
-        #print('flag')
 
         outer_break_flag = False
         while True:
             niter = niter + 1
-            #print('niter:', niter)
             if (niter > maxiter):
                 outer_break_flag = True
                 break
             nnew = nnew + 1
             f1 = f2
-            #print('f1:', f1)
             fvalues[niter-1] = f1
 # ---------------------------------------------------------------
             if (nnew > mturn):
@@ -311,27 +260,11 @@ def dgm(data, m, sample, min_dis_sample, ns, nc, w_record, nblocks): # 已加入
 # ---------------------------------------------------------------
             break_flag = False
             for ndg in range(1, nbundle+1):
-                # if nblocks == 2:
-                #     with open('./results/test.txt', 'a') as file:
-                #         file.write(f'\t dem_ndg\tg\t {ndg}\t {sl}\t {sample[0]}\t {sample[1]}\t {g[0]}\t {g[1]}\t {g[2]} \n')
-                # with open('./results/test.txt', 'a') as file:
-                #     file.write(f'\t dem_ndg\tsl\tx\t {ndg}\t {sl}\t {sample[0]}\t {sample[1]}\t {sample[2]} '
-                #                f'{sample[3]}\n')
                 v = dgrad(sample, sl, g, iact, lact3, data, nrecord, ns, m, w_record)
-                # if nblocks == 2:
-                #     with open('./results/test.txt', 'a') as file:
-                #         file.write(f' dgm_v: {v} \t \r\n')
-
                 dotprod = 0.0e+00
                 for i in range(0, m):
                     dotprod = dotprod + v[i] * v[i]
                 r = np.sqrt(dotprod)
-                # if nblocks == 2:
-                #     with open('./results/test.txt', 'a') as file:
-                #         file.write(f' dgm_r: {r} \t \r\n')
-
-                #if ndg == 2:
-                    #print('v', v)
                 if (r < eps0):
                     break_flag = True
                     break
@@ -345,13 +278,6 @@ def dgm(data, m, sample, min_dis_sample, ns, nc, w_record, nblocks): # 已加入
                         kmin = ndg
                     rmean = ((ndg-1)*rmean + r)/ndg
                 toler = max(eps0, dist1*rmean)
-                # if nblocks == 2:
-                #     with open('./results/test.txt', 'a') as file:
-                #         file.write(f' dgm_toler: {toler} \t \r\n')
-
-                # if ns == 2:
-                #     with open('./results/test.txt', 'a') as file:
-                #         file.write(f'\t dem_toler\t {toler}\n')
 
                 if ndg > 1:
                     for i in range(0, ndg-1):
@@ -365,14 +291,10 @@ def dgm(data, m, sample, min_dis_sample, ns, nc, w_record, nblocks): # 已加入
 
                 z, jvertex, ij = wolfe(ndg, prod, kmin, ns)
 
-                # if ns == 1 and nc == 5:
-                #     with open('./results/test.txt', 'a') as file:
-                #         file.write(f'\t dem_z\t {z[0]}\t {z[1]}\t {z[2]}\t\n')
                 for i in range(0, m):
                     v[i] = 0.0
                     for j in range(0, jvertex):
                         v[i] = v[i] + ww[ij[j]-1, i] * z[j]
-                #print('v', v)
                 r = 0
                 for i in range(0, m):
                     r = r + v[i] * v[i]
@@ -386,20 +308,11 @@ def dgm(data, m, sample, min_dis_sample, ns, nc, w_record, nblocks): # 已加入
                     x1[i] = sample[i] + sl * g[i]
 
                 f4, iact, lact3 = fv(ns, x1, data, min_dis_sample, nrecord, nc, w_record)
-                #if ndg == 1:
-                    #print('testfv')
                 f3 = (f4 - f1) / sl
                 decreas = step0 * r
                 if f3 < decreas:
-                    # if ns == 1 and nc == 5:
-                    #     with open('./results/test.txt', 'a') as file:
-                    #         file.write(f'\t run armijo\t f3=\t {f3} \n')
                     f5, step, iact, lact3 = armijo(sample, data, min_dis_sample, nrecord, g, f1, f4, sl, ns, r, nc, m, w_record)
-                    # if ns == 1 and nc == 5:
-                    #     with open('./results/test.txt', 'a') as file:
-                    #         file.write(f'\t dgm_f5\t {f5}\n')
                     f2 = f5
-                    #print('f2:', f2, ndg, jvertex)
                     for i in range(0, m):
                         sample[i] = sample[i] + step * g[i]
                     if ndg <= 2:
@@ -415,7 +328,6 @@ def dgm(data, m, sample, min_dis_sample, ns, nc, w_record, nblocks): # 已加入
         if outer_break_flag:
             break
 
-    #print('dgm', sample, f2)
     return sample, f2
 
 
